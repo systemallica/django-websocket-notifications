cb3b1a8d70e21b1af049e711c89001e6e53f3f5c
Andrés Reverón Molina <andres@reveronmolina.me>
2021-06-16 09:44:49 +0200

    chore: update to channels@3.0.3 + snitch@2.0.1

--- websocket_notifications/routing.py
+++ websocket_notifications/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from websocket_notifications.consumers import NotificationConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/notifications/(?P<code>.+)/$", NotificationConsumer)
+    re_path(r"ws/notifications/(?P<code>.+)/$", NotificationConsumer.as_asgi())
 ]
